813f377799bd642b5b49b566d69d8e68d9b103cc
Volodymyr Stelmashchuk <volodymyrstelmashchuk@Volodymyrs-MacBook-Pro.local>
2024-06-26 17:29:11 +0300

    add rest api, first try

--- lbf/src/main.rs
+++ lbf/src/main.rs
@@ -4,6 +4,7 @@ use std::io::BufReader;
 use std::path::Path;
 
 use clap::Parser as ClapParser;
+use jagua_rs::io::json_instance::JsonInstance;
 use log::{error, warn};
 use mimalloc::MiMalloc;
 use rand::prelude::SmallRng;
@@ -12,6 +13,7 @@ use rand::SeedableRng;
 use jagua_rs::io::parser;
 use jagua_rs::io::parser::Parser;
 use jagua_rs::util::polygon_simplification::PolySimplConfig;
+use serde::{Deserialize, Serialize};
 use lbf::io::cli::Cli;
 use lbf::io::json_output::JsonOutput;
 use lbf::io::layout_to_svg::s_layout_to_svg;
@@ -23,7 +25,7 @@ use lbf::{io, EPOCH};
 static GLOBAL: MiMalloc = MiMalloc;
 
 //more efficient allocator
-fn main() {
+fn mainOld() {
     let args = Cli::parse();
     io::init_logger(args.log_level);
 
@@ -49,7 +51,7 @@ fn main() {
         }
     };
 
-    let json_instance = io::read_json_instance(args.input_file.as_path());
+    let json_instance: JsonInstance = io::read_json_instance(args.input_file.as_path());
     let poly_simpl_config = match config.poly_simpl_tolerance {
         Some(tolerance) => PolySimplConfig::Enabled { tolerance },
         None => PolySimplConfig::Disabled,
@@ -98,3 +100,82 @@ fn main() {
         );
     }
 }
+
+use warp::Filter;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct RequestBody {
+    input: JsonInstance,
+    config: LBFConfig,
+}
+
+#[tokio::main]
+async fn main() {
+    // print a message
+    let run = warp::post()
+        .and(warp::path("run"))
+        .and(warp::body::json())
+        .map(|body: RequestBody| {
+            print!("Running");
+            // Here, you would call your existing CLI function with the provided arguments
+            // For the sake of this example, let's just return a dummy response
+
+            let json_instance = body.input;
+            let config = body.config;
+
+            let poly_simpl_config = match config.poly_simpl_tolerance {
+                Some(tolerance) => PolySimplConfig::Enabled { tolerance },
+                None => PolySimplConfig::Disabled,
+            };
+
+            let parser = Parser::new(poly_simpl_config, config.cde_config, true);
+            let instance = parser.parse(&json_instance);
+
+            let rng = match config.prng_seed {
+                Some(seed) => SmallRng::seed_from_u64(seed),
+                None => SmallRng::from_entropy(),
+            };
+
+            let mut optimizer = LBFOptimizer::new(instance.clone(), config, rng);
+            let solution = optimizer.solve();
+
+            let json_output = JsonOutput {
+                instance: json_instance.clone(),
+                solution: parser::compose_json_solution(&solution, &instance, EPOCH.clone()),
+                config: config.clone(),
+            };
+
+            let solution_folder = Path::new("solutions_test");
+
+            fs::create_dir_all(&solution_folder).unwrap_or_else(|_| {
+                panic!(
+                    "could not create solution folder: {:?}",
+                    solution_folder
+                )
+            });
+
+            let solution_path = solution_folder
+                .join(format!("sol_{}.json", '0'));
+
+            //panic!("solution path {}", solution_path.to_str().unwrap());
+
+            io::write_json_output(&json_output, Path::new(&solution_path));
+
+            for (i, s_layout) in solution.layout_snapshots.iter().enumerate() {
+                let svg_path = solution_folder
+                    .join(format!("sol_{}_{}.svg", "input_file_stem", i));
+
+                io::write_svg(
+                    &s_layout_to_svg(s_layout, &instance, config.svg_draw_options),
+                    Path::new(&svg_path),
+                );
+            }
+
+            "tset"
+        });
+
+    warp::serve(run)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
