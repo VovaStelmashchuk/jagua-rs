d4c64c25161f0d6d3a397182918b134bed8ef27c
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-08-18 23:30:11 +0300

    update main.rs to have json an single input

--- lbf/src/main.rs
+++ lbf/src/main.rs
@@ -1,158 +1,112 @@
-use std::fs;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::{Path, PathBuf};
-
+use std::io::{self, Write, Read};
 use anyhow::{Context, Result};
-use clap::Parser as ClapParser;
 use jagua_rs::io::import::Importer;
-use jagua_rs::io::svg::s_layout_to_svg;
 use jagua_rs::probs::bpp::io::ext_repr::ExtBPInstance;
 use jagua_rs::probs::spp::io::ext_repr::ExtSPInstance;
 use jagua_rs::probs::{bpp, spp};
 use lbf::config::LBFConfig;
-use lbf::io::cli::{Cli, ProblemVariant};
 use lbf::io::output::{BPOutput, SPOutput};
-use lbf::io::{read_bpp_instance, read_spp_instance};
 use lbf::opt::lbf_bpp::LBFOptimizerBP;
 use lbf::opt::lbf_spp::LBFOptimizerSP;
-use lbf::{EPOCH, io};
-use log::{info, warn};
+use lbf::EPOCH;
+use log::info;
 use rand::SeedableRng;
 use rand::prelude::SmallRng;
+use serde_json::{self, Value};
 
-//more efficient allocator
-fn main() -> Result<()> {
-    let args = Cli::parse();
-    io::init_logger(args.log_level)?;
-
-    let config = match args.config_file {
-        None => {
-            warn!("[MAIN] No config file provided, use --config-file to provide a custom config");
-            LBFConfig::default()
-        }
-        Some(config_file) => {
-            let file = File::open(config_file)?;
-            let reader = BufReader::new(file);
-            serde_json::from_reader(reader).context("incorrect config file format")?
-        }
-    };
-
-    info!("Successfully parsed LBFConfig: {config:?}");
-
-    let input_file_stem = args.input_file.file_stem().unwrap().to_str().unwrap();
-
-    if !args.solution_folder.exists() {
-        fs::create_dir_all(&args.solution_folder).unwrap_or_else(|_| {
-            panic!(
-                "could not create solution folder: {:?}",
-                args.solution_folder
-            )
-        });
-    }
-
-    match args.prob_var {
-        ProblemVariant::BinPackingProblem => {
-            let ext_bp_instance = read_bpp_instance(args.input_file.as_path())?;
-            main_bpp(
-                ext_bp_instance,
-                config,
-                input_file_stem,
-                args.solution_folder,
-            )
-        }
-        ProblemVariant::StripPackingProblem => {
-            let ext_sp_instance = read_spp_instance(args.input_file.as_path())?;
-            main_spp(
-                ext_sp_instance,
-                config,
-                input_file_stem,
-                args.solution_folder,
-            )
-        }
-    }
+#[derive(serde::Deserialize)]
+struct InputData {
+    config: LBFConfig,
+    problem_type: String,
+    instance: Value,
 }
 
-fn main_spp(
-    ext_instance: ExtSPInstance,
-    config: LBFConfig,
-    input_stem: &str,
-    output_folder: PathBuf,
-) -> Result<()> {
-    let importer = Importer::new(
-        config.cde_config,
-        config.poly_simpl_tolerance,
-        config.min_item_separation,
-        config.narrow_concavity_cutoff_ratio,
-    );
-    let rng = match config.prng_seed {
-        Some(seed) => SmallRng::seed_from_u64(seed),
-        None => SmallRng::from_os_rng(),
+#[derive(serde::Serialize)]
+struct OutputData {
+    success: bool,
+    error: Option<String>,
+    solution: Option<Value>,
+    config: Option<LBFConfig>,
+}
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    let mut stdin = io::stdin();
+    stdin.read_to_string(&mut input)?;
+    
+    let input_data: InputData = serde_json::from_str(&input)
+        .context("Failed to parse input JSON")?;
+    
+    let config = input_data.config;
+    let problem_type = input_data.problem_type.to_lowercase();
+    
+    info!("Successfully parsed LBFConfig: {config:?}");
+    info!("Problem type: {problem_type}");
+    
+    let result = match problem_type.as_str() {
+        "bpp" | "bin_packing" | "binpacking" => {
+            let ext_bp_instance: ExtBPInstance = serde_json::from_value(input_data.instance)
+                .context("Failed to parse BPP instance")?;
+            solve_bpp(ext_bp_instance, config)
+        }
+        _ => {
+            let output = OutputData {
+                success: false,
+                error: Some(format!("Unknown problem type: {}", problem_type)),
+                solution: None,
+                config: None,
+            };
+            serde_json::to_writer_pretty(io::stdout(), &output)?;
+            return Ok(());
+        }
     };
-    let instance = spp::io::import(&importer, &ext_instance)?;
-    let sol = LBFOptimizerSP::new(instance.clone(), config, rng).solve();
-
-    {
-        let output = SPOutput {
-            instance: ext_instance,
-            solution: spp::io::export(&instance, &sol, *EPOCH),
-            config,
-        };
-
-        let solution_path = output_folder.join(format!("sol_{input_stem}.json"));
-
-        io::write_json(&output, Path::new(&solution_path))?;
+    
+    match result {
+        Ok(output) => {
+            serde_json::to_writer_pretty(io::stdout(), &output)?;
+        }
+        Err(e) => {
+            let error_output = OutputData {
+                success: false,
+                error: Some(e.to_string()),
+                solution: None,
+                config: None,
+            };
+            serde_json::to_writer_pretty(io::stdout(), &error_output)?;
+        }
     }
-
-    {
-        let svg_path = output_folder.join(format!("sol_{input_stem}.svg"));
-        let svg = s_layout_to_svg(&sol.layout_snapshot, &instance, config.svg_draw_options, "");
-
-        io::write_svg(&svg, Path::new(&svg_path))?;
-    }
-
+    
     Ok(())
 }
 
-fn main_bpp(
+fn solve_bpp(
     ext_instance: ExtBPInstance,
     config: LBFConfig,
-    input_stem: &str,
-    output_folder: PathBuf,
-) -> Result<()> {
+) -> Result<OutputData> {
     let importer = Importer::new(
         config.cde_config,
         config.poly_simpl_tolerance,
         config.min_item_separation,
         config.narrow_concavity_cutoff_ratio,
     );
+    
     let rng = match config.prng_seed {
         Some(seed) => SmallRng::seed_from_u64(seed),
         None => SmallRng::from_os_rng(),
     };
+    
     let instance = bpp::io::import(&importer, &ext_instance)?;
-    let sol = LBFOptimizerBP::new(instance.clone(), config, rng).solve();
-
-    {
-        let output = BPOutput {
-            instance: ext_instance,
-            solution: bpp::io::export(&instance, &sol, *EPOCH),
-            config,
-        };
-
-        let solution_path = output_folder.join(format!("sol_{input_stem}.json"));
-
-        io::write_json(&output, Path::new(&solution_path))?;
-    }
-
-    {
-        for (i, s_layout) in sol.layout_snapshots.values().enumerate() {
-            let svg_path = output_folder.join(format!("sol_{input_stem}_{i}.svg"));
-            let svg = s_layout_to_svg(s_layout, &instance, config.svg_draw_options, "");
-
-            io::write_svg(&svg, Path::new(&svg_path))?;
-        }
-    }
-
-    Ok(())
+    let sol = LBFOptimizerBP::new(instance.clone(), config.clone(), rng).solve();
+    
+    let solution = bpp::io::export(&instance, &sol, *EPOCH);
+    let solution_value = serde_json::to_value(solution)?;
+    
+    let output = OutputData {
+        success: true,
+        error: None,
+        solution: Some(solution_value),
+        config: Some(config),
+    };
+    
+    Ok(output)
 }
